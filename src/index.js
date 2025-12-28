830171fb55c1b6dd72339716b908ea260dd9ac9a
damipic100-sys <damipic100@gmail.com>
2025-12-28 13:01:04 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ export default {
       "https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/NOMBRE",
       {
         headers: {
-          "X-Riot-Token": env.RIOT_API_KEY
+          "X-Riot-Token": env.RGAPI-5e226843-f286-4190-bac3-9561e757101e
         }
       }
     );
+    const summoners = ["DAMI#ARG", "RaviolesConCrema#Queso", "N1GHTMA#zzz"];
+
 
     const data = await res.json();
     return Response.json(data);
